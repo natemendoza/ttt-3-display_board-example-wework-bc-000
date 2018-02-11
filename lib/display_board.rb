a3a94a3ac5b8d33f5c2c437131594c9ead255af5
Nathaniel Mendoza <nathaniel.mendoza@wework.com>
2018-02-11 23:54:25 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,10 @@
 
 def display_board
   puts "A Tic Tac Toe Board"
-puts    |   |   
-puts -----------
-puts    |   |   
-puts -----------
+puts "    |   |  "
+puts "-----------"
+puts "    |   |  "
+puts "-----------"
 puts    |   |   
 end
 
